f654b657016b514f5c69da80a71c79b61a4e219f
Kanchanop6509650229 <kanchanop.bua@dome.tu.ac.th>
2024-10-15 10:22:52 +0700

    first commit

---
+++ src/__test__/math.test.js
@@ -0,0 +1,19 @@
+const math = require("../math");
+
+describe("simple arithmetic", () => {
+  describe("add", () => {
+    it("expect 5 + 3 = 8", async () => {
+      expect(math.add(5, 3)).toBe(8);
+    });
+  });
+  describe("subtract", () => {
+    it("expect 5 - 1 = 4", async () => {
+      expect(math.subtract(5, 1)).toBe(4);
+    });
+  });
+  describe("multiply", () => {
+    it("expect 2 * 3 = 6", async () => {
+      expect(math.multiply(2, 3)).toBe(6);
+    });
+  });
+});
